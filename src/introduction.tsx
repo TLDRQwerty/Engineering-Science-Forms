ecbe2ace1d35545d7cbc53a3b82ca255c02062b1
Charlie Carolan <charliecarolan1999@gmail.com>
2021-04-28 17:07:19 +0100

    stuff and more things

--- src/introduction.tsx
+++ src/introduction.tsx
@@ -8,6 +8,20 @@ export default function Introduction(): JSX.Element {
 			questionnaire.  Please use the Google Form Link below:
 			<br/>
 			<Link to="https://forms.gle/hvDF3ULaVuwBSbGB8" label="Google Form Link" />
+			<section className="px-16 mt-4 select-none">
+				<div className="flex justify-between">
+					<span>Card Holder name</span><span>John Smith</span>
+				</div>
+				<div className="flex justify-between">
+					<span>Card number</span><span>4242 4242 4242 4242</span>
+				</div>
+				<div className="flex justify-between">
+					<span>Expiration Date</span><span>06/2020</span>
+				</div>
+				<div className="flex justify-between">
+					<span>CVC</span><span>0321</span>
+				</div>
+			</section>
 		</div>
 	);
 }
